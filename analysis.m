c37dc5fa8b5783a40d8d21746c57ab0cc0651a0b
cosmo <cosmo@cosmo.red>
2020-08-03 21:42:29 -0400

    Add analysis of model dynamics

---
+++ analysis.m
@@ -0,0 +1,33 @@
+% set user-specific parameters
+modelparams = '/home/cosmo/code/liu-control/conductances.mat';
+cachefolder = '/home/cosmo/code/liu-control/cache/';
+
+% load provided models
+load(modelparams) % add "conductances" to workspace
+
+% initialize data structures
+stabs = zeros(length(conductances), 1);
+newmets = repmat(xtools.V2metrics(0), length(conductances), 1);
+oldmets = repmat(xtools.V2metrics(0), length(conductances), 1);
+
+% determine stability of model dynamics with liu_stability
+parfor i = 1:newlength(conductances)
+    hash = hashlib.md5hash(conductances(i,:));
+    if isfile(join([cachefolder,hash,'.mat']))
+        temp = load(join([cachefolder,hash,'.mat']));
+        stabs(i,1) = temp.stability;
+        newmets(i,1) = temp.newmetrics;
+        oldmets(i,1) = temp.oldmetrics;
+    else
+        [stability,newmetrics,oldmetrics] = liu_stability(conductances(i,:));
+        parsave(join([cachefolder,hash,'.mat']),stability,newmetrics,oldmetrics);
+        stabs(i,1) = stability;
+        newmets(i,1) = newmetrics;
+        oldmets(i,1) = oldmetrics;
+    end
+end
+
+% workaround for saving in parfor loops
+function [] = parsave(filename,stability,newmetrics,oldmetrics)
+save(filename,'stability','newmetrics','oldmetrics');
+end
