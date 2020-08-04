510c2d64291aa23e117256c11ce354ba98511a30
cosmo <cosmo@cosmo.red>
2020-08-04 17:35:02 -0400

    Remove hardcoded path to cache and fix #3

--- analysis.m
+++ analysis.m
@@ -1,5 +1,3 @@
-cachefolder = '/home/cosmo/code/liu-control/cache/';
-
 % load provided models
 load('conductances.mat')
 
@@ -11,14 +9,14 @@ oldmets = repmat(xtools.V2metrics(0), length(conductances), 1);
 % determine stability of model dynamics with liu_stability
 parfor i = 1:length(conductances)
     hash = hashlib.md5hash(conductances(i,:));
-    if isfile(fullfile(cachefolder,[hash,'.mat']))
-        temp = load(fullfile(cachefolder,[hash,'.mat']));
+    if isfile(fullfile('cache',[hash,'.mat']))
+        temp = load(fullfile('cache',[hash,'.mat']));
         stabs(i,1) = temp.stability;
         newmets(i,1) = temp.newmetrics;
         oldmets(i,1) = temp.oldmetrics;
     else
         [stability,newmetrics,oldmetrics] = liu_stability(conductances(i,:));
-        parsave(fullfile(cachefolder,[hash,'.mat']),stability,newmetrics,oldmetrics);
+        parsave(fullfile('cache',[hash,'.mat']),stability,newmetrics,oldmetrics);
         stabs(i,1) = stability;
         newmets(i,1) = newmetrics;
         oldmets(i,1) = oldmetrics;
